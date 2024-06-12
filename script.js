9e4c4567ff6b90b31e92857767db31e9a8264a80
bwondessen <bwondessen23@gmail.com>
2024-06-12 00:36:53 -0500

    If user enters an invalid input, alert them and return null

--- script.js
+++ script.js
@@ -38,6 +38,12 @@ function getCaloriesFromInputs(list) {
     for (const item of list) {
         const currVal = cleanInputString(item.value);
         const invalidInputMatch = isInvalidInput(currVal);
+
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
     }
 }
 
